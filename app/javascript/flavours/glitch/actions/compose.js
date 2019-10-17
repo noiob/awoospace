f07b7f3b32ecac4330f1061f3e92d650d11b4693
noiob <8197071+noiob@users.noreply.github.com>
2019-10-17 22:03:57 +0200

    Add awoospace branding

--- app/javascript/flavours/glitch/actions/compose.js
+++ app/javascript/flavours/glitch/actions/compose.js
@@ -147,7 +147,7 @@ export function submitCompose(routerHistory) {
 
     dispatch(submitComposeRequest());
     if (getState().getIn(['compose', 'advanced_options', 'do_not_federate'])) {
-      status = status + ' 👁️';
+      status = status + ' 🐺';
     }
     api(getState).post('/api/v1/statuses', {
       status,
