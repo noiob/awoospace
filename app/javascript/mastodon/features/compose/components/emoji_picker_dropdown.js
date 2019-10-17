e72ce8e779b957af5a256104644f67dc37a2e318
noiob <8197071+noiob@users.noreply.github.com>
2019-10-17 22:03:57 +0200

    Add awoospace branding

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
@@ -378,8 +378,8 @@ class EmojiPickerDropdown extends React.PureComponent {
         <div ref={this.setTargetRef} className='emoji-button' title={title} aria-label={title} aria-expanded={active} role='button' onClick={this.onToggle} onKeyDown={this.onToggle} tabIndex={0}>
           {button || <img
             className={classNames('emojione', { 'pulse-loading': active && loading })}
-            alt='🙂'
-            src={`${assetHost}/emoji/1f602.svg`}
+            alt='🐺'
+            src={`${assetHost}/emoji/1f43a.svg`}
           />}
         </div>
 
