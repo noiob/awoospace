6366b4597505a096c7fc8836ee90a19ff9b7e9c2
noiob <8197071+noiob@users.noreply.github.com>
2019-10-17 21:08:43 +0200

    Add awoo locale

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,7 @@ module Mastodon
     # All translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
+      :'en-AW',
       :af,
       :ar,
       :ast,
