9ce58306d4e8ca748a893e665b5b94ecd8d80ace
noiob <8197071+noiob@users.noreply.github.com>
2019-10-21 22:26:01 +0200

    Increase toot length limit to 16384 when a CW is used

--- app/validators/status_length_validator.rb
+++ app/validators/status_length_validator.rb
@@ -4,6 +4,9 @@ class StatusLengthValidator < ActiveModel::Validator
   MAX_CHARS = (ENV['MAX_TOOT_CHARS'] || 500).to_i
 
   def validate(status)
+    status.errors.add(:text, "hard limit of 16k exceeded") if status.text.length > 16384
+    status.errors.add(:text, "CW can't be longer than 500 characters") if status.spoiler_text.length > 500
+    return unless status.spoiler_text.blank?
     return unless status.local? && !status.reblog?
 
     @status = status
