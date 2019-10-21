04d33af4a7f77116140becb88541fa6e52bfc11b
noiob <8197071+noiob@users.noreply.github.com>
2019-10-21 22:26:01 +0200

    Increase toot length limit to 16384 when a CW is used

--- app/javascript/flavours/glitch/features/compose/components/compose_form.js
+++ app/javascript/flavours/glitch/features/compose/components/compose_form.js
@@ -94,8 +94,9 @@ class ComposeForm extends ImmutablePureComponent {
   canSubmit = () => {
     const { isSubmitting, isChangingUpload, isUploading, anyMedia } = this.props;
     const fulltext = this.getFulltextForCharacterCounting();
+    const no_limit = this.props.spoiler && this.props.spoilerText != '';
 
-    return !(isSubmitting || isUploading || isChangingUpload || length(fulltext) > maxChars || (!fulltext.trim().length && !anyMedia));
+    return !(isSubmitting || isUploading || isChangingUpload || (length(fulltext) > maxChars && !no_limit) || (!fulltext.trim().length && !anyMedia));
   }
 
   handleSubmit = (overriddenVisibility = null) => {
@@ -296,6 +297,7 @@ class ComposeForm extends ImmutablePureComponent {
     } = this.props;
 
     const countText = this.getFulltextForCharacterCounting();
+    const no_limit = spoiler && spoilerText != '';
 
     return (
       <div className='composer'>
@@ -357,7 +359,7 @@ class ComposeForm extends ImmutablePureComponent {
             spoiler={spoilersAlwaysOn ? (spoilerText && spoilerText.length > 0) : spoiler}
           />
           <div className='compose--counter-wrapper'>
-            <CharacterCounter text={countText} max={maxChars} />
+            <CharacterCounter text={countText} max={maxChars} no_limit={no_limit} />
           </div>
         </div>
 
